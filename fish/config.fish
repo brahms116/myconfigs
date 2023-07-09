f0925e55f8ee482147e569613de5a304d1d66200
David Kwong <20544dk@gmail.com>
2023-07-10 08:35:47 +1000

    Add ec2 describe alias

--- fish/config.fish
+++ fish/config.fish
@@ -42,5 +42,6 @@ if status is-interactive
     alias lg=lazygit
     alias cl=clear
     alias vi=nvim
+    alias ec2='aws ec2 describe-instances --query "Reservations[].Instances[].[Tags[?Key==`Name`]|[0].Value,PublicIpAddress,State.Name]" --output table'
 end
 
