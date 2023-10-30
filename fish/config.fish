3ae679250ea313aae399021f3685eda1f1a0aaee
David Kwong <20544dk@gmail.com>
2023-10-31 09:30:06 +1000

    Add tmuxinator and move session binding somewhere else

--- fish/config.fish
+++ fish/config.fish
@@ -53,4 +53,6 @@ if status is-interactive
     alias vi=nvim
     alias ec2='aws ec2 describe-instances --query "Reservations[].Instances[].[Tags[?Key==`Name`]|[0].Value,PublicIpAddress,State.Name]" --output table'
     alias config='cd ~/dotfiles'
+    alias mx='tmuxinator'
+    alias x='tmux'
 end
