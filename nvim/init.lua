cde901e00d080f36987770c2b798daecaca4505d
David Kwong <20544dk@gmail.com>
2024-09-06 09:06:41 +1000

    Replace deprecated call for omnifunc

--- nvim/init.lua
+++ nvim/init.lua
@@ -262,7 +262,7 @@ local function setup(settings)
 
 
     -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+    vim.bo[bufnr].omnifunc = 'v:lua.vim.lsp.omnifunc'
     -- Mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local bufopts = { noremap = true, silent = true, buffer = bufnr }
