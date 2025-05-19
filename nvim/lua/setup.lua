3fe1c7d764a1c8bac5bfbda0d6db7ad0ffd420cf
David Kwong <20544dk@gmail.com>
2025-05-20 08:42:38 +1000

    try incremental selection

--- nvim/lua/setup.lua
+++ nvim/lua/setup.lua
@@ -354,61 +354,13 @@ function M.setup(settings)
     auto_install = false,
     ensure_installed = {},
     ignore_install = {},
-    textobjects = {
-      select = {
-        enable = true,
-
-        -- Automatically jump forward to textobj, similar to targets.vim
-        lookahead = true,
-
-        keymaps = {
-          ["ls"] = "@assignment.lhs",
-          ["rs"] = "@assignment.rhs",
-
-          ["aa"] = "@assignment.outer",
-          ["ia"] = "@assignment.inner",
-
-          ["ap"] = "@parameter.outer",
-          ["ip"] = "@parameter.inner",
-
-          ["aC"] = "@call.outer",
-          ["iC"] = "@call.inner",
-
-          ["al"] = "@loop.outer",
-          ["il"] = "@loop.inner",
-
-          ["af"] = "@function.outer",
-          ["if"] = "@function.inner",
-
-          ["ac"] = "@class.outer",
-          -- You can optionally set descriptions to the mappings (used in the desc parameter of
-          -- nvim_buf_set_keymap) which plugins like which-key display
-          ["ic"] = { query = "@class.inner", desc = "Select inner part of a class region" },
-          -- You can also use captures from other query groups like `locals.scm`
-          ["as"] = { query = "@local.scope", query_group = "locals", desc = "Select language scope" },
-        },
-        -- You can choose the select mode (default is charwise 'v')
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * method: eg 'v' or 'o'
-        -- and should return the mode ('v', 'V', or '<c-v>') or a table
-        -- mapping query_strings to modes.
-        selection_modes = {
-          ['@parameter.outer'] = 'v', -- charwise
-          ['@function.outer'] = 'V',  -- linewise
-          ['@class.outer'] = 'V'
-        },
-        -- If you set this to `true` (default is `false`) then any textobject is
-        -- extended to include preceding or succeeding whitespace. Succeeding
-        -- whitespace has priority in order to act similarly to eg the built-in
-        -- `ap`.
-        --
-        -- Can also be a function which gets passed a table with the keys
-        -- * query_string: eg '@function.inner'
-        -- * selection_mode: eg 'v'
-        -- and should return true or false
-        include_surrounding_whitespace = false,
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<CR>',
+        node_incremental = '<CR>',
+        scope_incremental = '<TAB>',
+        node_decremental = '<S-TAB>',
       },
     },
   }
